8dfd7d449b5beb37637e6f116a5b4c87b1f0121a
Shelley Chen <shelleychen318@gmail.com>
2023-01-28 20:33:10 -0500

    added home image

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,17 @@ export default function Home() {
       <NavBar />
 
       <div className={styles.banner}>
-        <h1>SYDE 2027 Class Profile!</h1>
-        <a className={styles.button} href="/demographics">
-          Go to Profile
-        </a>
+        <div className={styles.title}>
+          <h1>Systems Design Engineering 2027 Class Profile!</h1>
+          <a className={styles.button} href="/demographics">
+            Go to Profile
+          </a>
+        </div>
+        <img
+          src="/assets/home_img.png"
+          alt="home image"
+          className={styles.image}
+        />
       </div>
     </>
   );
