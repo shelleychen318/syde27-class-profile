978aa799a3b604ebb07a901b2c3a980f0d1b8d1c
Shelley Chen <shelleychen318@gmail.com>
2023-02-19 11:56:36 -0500

    minor refractoring

--- components/index.js
+++ components/index.js
@@ -1,4 +1,4 @@
-export { default as NavBar } from "./navbar-1/NavBar";
+export { default as NavBar } from "./navbar/NavBar";
 export { default as PieChart } from "./charts/PieChart";
 export { default as Chart } from "./charts/Chart"
 
