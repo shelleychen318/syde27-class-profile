11dafe92914bb1e5aa850b65854b9dd3102e612b
ndough <106121863+ndough@users.noreply.github.com>
2023-03-13 21:36:56 -0400

    testing for highschool page

--- pages/highschool/index.js
+++ pages/highschool/index.js
@@ -1,8 +1,62 @@
 import Head from "next/head";
 import { NavBar } from "../../components";
 import styles from "../profile.module.scss";
+import { Chart } from "../../components";
+import { PieChart } from "../../components";
+import { getSortedHighschoolData } from "../../lib/sort/getSortedHighschoolData";
 
-export default function HighSchool() {
+export const getStaticProps = async () => {
+  const data = await getSortedHighschoolData();
+  return {
+    props: {
+      ...data,
+    },
+  };
+};
+
+export default function Highschool(data) {
+  const colorTheme1 = ["#173753", "#6DAEDB", "#2892D7", "#1D70A2"];
+
+  const averageData = {
+    label: data.averages,
+    val: data.averageValues,
+    color: colorTheme1,
+    title: "Average",
+    n: data.averageRespondents, // number of respondents
+  };
+
+  const extraNumData = {
+    label: data.extracurricularNum,
+    val: data.extraNumValues,
+    color: colorTheme1,
+    title: "Extracurriculars",
+    n: data.extraNumRespondents,
+  };
+
+  // const ethnicityData = {
+  //   label: data.ethnicities,
+  //   val: data.ethnicityValues,
+  //   color: colorTheme[1],
+  //   title: "Ethnicities",
+  //   n: data.ethnicityRespondents,
+  // };
+
+  // const religionData = {
+  //   label: data.religions,
+  //   val: data.religionValues,
+  //   color: colorTheme[1],
+  //   title: "Religions",
+  //   n: data.religionRespondents,
+  // };
+
+  // const hometownData = {
+  //   label: data.hometowns,
+  //   val: data.hometownValues,
+  //   color: colorTheme[1],
+  //   title: "Hometowns",
+  //   n: data.hometownRespondents,
+  // };
+
   return (
     <>
       <Head>
@@ -20,8 +74,16 @@ export default function HighSchool() {
           </h4>
           <br />
           <h3>High School Average</h3>
+
+          <h3>Averages</h3>
+
+          <div className={styles.doubleChart}>
+            <Chart type="pie" data={averageData} position="left" />
+            <Chart type="pie" data={extraNumData} position="right" />
+          </div>
         </div>
       </div>
+
     </>
   );
 }
