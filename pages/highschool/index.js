ff806c860d709ddb5cdb88c38092834d35c52627
ndough <106121863+ndough@users.noreply.github.com>
2023-03-14 13:49:46 -0400

    upload all data for high school

--- pages/highschool/index.js
+++ pages/highschool/index.js
@@ -33,29 +33,77 @@ export default function Highschool(data) {
     n: data.extraNumRespondents,
   };
 
-  // const ethnicityData = {
-  //   label: data.ethnicities,
-  //   val: data.ethnicityValues,
-  //   color: colorTheme[1],
-  //   title: "Ethnicities",
-  //   n: data.ethnicityRespondents,
-  // };
+  const extraTypeData = {
+    label: data.extracurricularType,
+    val: data.extraTypeValues,
+    color: colorTheme1,
+    title: "Type of Extracurriculars",
+    n: data.extraTypeRespondents,
+  };
 
-  // const religionData = {
-  //   label: data.religions,
-  //   val: data.religionValues,
-  //   color: colorTheme[1],
-  //   title: "Religions",
-  //   n: data.religionRespondents,
-  // };
+  const extraRoleData = {
+    label: data.extraRoles,
+    val: data.extraRoleValues,
+    color: colorTheme1,
+    title: "Roles in Extracurriculars",
+    n: data.extraRoleRespondents,
+  };
 
-  // const hometownData = {
-  //   label: data.hometowns,
-  //   val: data.hometownValues,
-  //   color: colorTheme[1],
-  //   title: "Hometowns",
-  //   n: data.hometownRespondents,
-  // };
+  const enrichData = {
+    label: data.enrichs,
+    val: data.enrichValues,
+    color: colorTheme1,
+    title: "Enrichement Data",
+    n: data.enrichsRespondents,
+  };
+
+    const postChoiceData = {
+    label: data.postChoice,
+    val: data.postChoicesValues,
+    color: colorTheme1,
+    title: "Post Secondary Choices",
+    n: data.postChoicesRespondents,
+  };
+
+  const sydeBoolData = {
+    label: data.sydeBools,
+    val: data.sydeBoolValues,
+    color: colorTheme1,
+    title: "Yes or No to Syde?",
+    n: data.sydeBoolRespondents,
+  };
+
+  const topChoiceData = {
+    label: data.topChoices,
+    val: data.topChoiceValues,
+    color: colorTheme1,
+    title: "Top Choices",
+    n: data.topChoiceRespondents,
+  };
+
+    const considerData = {
+    label: data.considers,
+    val: data.considerValues,
+    color: colorTheme1,
+    title: "Consideration",
+    n: data.considerRespondents,
+  };
+
+  const decisionData = {
+    label: data.decisions,
+    val: data.decisionValues,
+    color: colorTheme1,
+    title: "Decision",
+    n: data.decisionRespondents,
+  };
+
+  const prepData = {
+    label: data.preparation,
+    val: data.prepareValues,
+    color: colorTheme1,
+    title: "Prep Level",
+    n: data.prepareRespondents,
+  };
 
   return (
     <>
@@ -80,6 +128,30 @@ export default function Highschool(data) {
           <div className={styles.doubleChart}>
             <Chart type="pie" data={averageData} position="left" />
             <Chart type="pie" data={extraNumData} position="right" />
+          </div>
+
+          <div className={styles.doubleChart}>
+            <Chart type="pie" data={extraTypeData} position="left" />
+            <Chart type="pie" data={extraRoleData} position="right" />
+          </div>
+
+          <div className={styles.doubleChart}>
+            <Chart type="pie" data={enrichData} position="left" />
+            <Chart type="pie" data={postChoiceData} position="right" />
+          </div>
+
+          <div className={styles.doubleChart}>
+            <Chart type="pie" data={sydeBoolData} position="left" />
+            <Chart type="pie" data={topChoiceData} position="right" />
+          </div>
+
+          <div className={styles.doubleChart}>
+            <Chart type="pie" data={considerData} position="left" />
+            <Chart type="pie" data={decisionData} position="right" />
+          </div>
+
+          <div className={styles.doubleChart}>
+            <Chart type="pie" data={prepData} position="right" />
           </div>
         </div>
       </div>
